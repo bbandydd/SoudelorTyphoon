27d17ce399d401d47c73bd4e5ed187a25f0ad25f
bbandydd <bbandydd@gmail.com>
2015-08-07 21:59:48 +0800

    修改區域

--- js/Controllers/MainController.js
+++ js/Controllers/MainController.js
@@ -7,7 +7,7 @@ app.controller('MainController', ['$scope', 'disaster', function($scope, disaste
 			console.log($scope.disasterList);
 
 			//區域
-			$scope.areas = ['中正區', '大同區', '中山區', '松山區', '大安區', '萬華區', '信義區', '士林區', 
+			$scope.areas = ['中山區', '大同區', '中山區', '松山區', '大安區', '萬華區', '信義區', '士林區', 
 				'北投區', '內湖區', '南港區', '文山區'];
 		})
 		.error(function(err){
